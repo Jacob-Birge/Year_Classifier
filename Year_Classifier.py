4d0a8bb57bfd4f062ff453b6a3c753fa7b54bb6a
Jacob-Birge <35941365+Jacob-Birge@users.noreply.github.com>
2021-02-08 11:17:57 -0500

    Setting up dataset and loader

--- Year_Classifier.py
+++ Year_Classifier.py
@@ -1,5 +1,4 @@
 from PIL import Image
-import pytorch_lightning as pl
 from os import listdir
 from os.path import isfile, join
 import sys
@@ -13,33 +12,32 @@ import numpy as np
 import torchvision
 from torchvision import *
 from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, datasets
 
 import matplotlib.pyplot as plt
 import time
 import copy
 import os
 
-#data is stored in format ./Data/{x_bound}/{y_bound}/{file}.tif
+#data is stored in format ./Data/{lat}_{long}.tif
 dataPath = "./Data"
-sourcePathsByYear = {}
-yearDirs = [d for d in listdir(dataPath) if not isfile(join(dataPath, d))]
-for yearDir in yearDirs:
-  sourcePathsByYear[yearDir] = []
-  yearDirFull = join(dataPath, yearDir)
-  xDirs = [join(yearDirFull, d) for d in listdir(yearDirFull) if not isfile(join(yearDirFull, d))]
-  values = range(len(xDirs))
-  with tqdm(total=len(values), file=sys.stdout) as pbar:
-    i=1
-    for xDir in xDirs:
-      yDirs = [join(xDir, d) for d in listdir(xDir) if not isfile(join(xDir, d))]
-      for yDir in yDirs:
-        files = [join(yDir, f) for f in listdir(yDir) if isfile(join(yDir, f))]
-        sourcePathsByYear[yearDir] += files
-      pbar.set_description('processed: %d' % (i))
-      i+=1
-      pbar.update(1)
-  print(len(sourcePathsByYear[yearDir]))
+data_transform = transforms.Compose([
+    transforms.RandomSizedCrop(224),
+    transforms.ToTensor()
+    #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+  ])
+dataset = datasets.ImageFolder(root=dataPath, transform=data_transform)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=True, num_workers=4)
 
-im = Image.open('./Data/2020/440000/4974000/440000.0_4974000.0.tif')
-train_data = np.array( [np.asarray(Image.open(sourcePathsByYear["2020"][i])) for i in range(len(sourcePathsByYear["2020"]))] )
-print(train_data.shape)
+for i_batch, sample_batched in enumerate(dataloader):
+  print(sample_batched)
+  print(i_batch, sample_batched['image'].size())
+
+  # observe 4th batch and stop.
+  if i_batch == 3:
+    plt.figure()
+    plt.imshow(sample_batched)
+    plt.axis('off')
+    plt.ioff()
+    plt.show()
+    break
