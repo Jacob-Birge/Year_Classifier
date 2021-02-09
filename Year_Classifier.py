674ddd213e8f402b8e6a480f8c9badb51cbde7df
Jacob-Birge <35941365+Jacob-Birge@users.noreply.github.com>
2021-02-08 19:08:39 -0500

    Update Year_Classifier.py

--- Year_Classifier.py
+++ Year_Classifier.py
@@ -20,6 +20,18 @@ import copy
 import os
 import tensorflow as tf
 
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
 #data is stored in format ./Data/{lat}_{long}.tif
 dataPath = "./Data"
 data_transform = transforms.Compose([
@@ -28,17 +40,13 @@ data_transform = transforms.Compose([
   #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 dataset = datasets.ImageFolder(root=dataPath, transform=data_transform)
+class_names = dataset.classes
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=True, num_workers=4)
 
-for i_batch, (sample_batched, idk) in enumerate(dataloader):
+for i_batch, (inputs, classes) in enumerate(dataloader):
   # observe 4th batch and stop.
   if (i_batch == 3):
-    print(idk)
-    print(sample_batched)
-    print(tf.shape(sample_batched[0]))
-    plt.figure()
-    plt.imshow(sample_batched)
-    plt.axis('off')
-    plt.ioff()
-    plt.show()
-    break
+    print(classes)
+    print(tf.shape(inputs))
+    out = torchvision.utils.make_grid(inputs)
+    imshow(out, title=[class_names[x] for x in classes])
